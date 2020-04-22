49f4c024afa56f9ba28d800efdf7488beb5a1a4e
Hong Kee Sul <hongkee.sul@gmail.com>
2020-04-22 19:52:03 +0900

    d

--- js/data.js
+++ js/data.js
@@ -16,3 +16,83 @@ var positions = [
         latlng: new kakao.maps.LatLng(33.451393, 126.570738)
     }
 ];
+
+
+var dataPositions = {
+    "option1": {
+        'convenience': [
+            {
+                title: '카카오', 
+                latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+            },
+            {
+                title: '생태연못', 
+                latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+            },
+            {
+                title: '텃밭', 
+                latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+            },
+            {
+                title: '근린공원',
+                latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+            }
+        ],
+        'coffee': [
+            {
+                title: 'coffee1', 
+                latlng: new kakao.maps.LatLng(34.450705, 126.570677)
+            },
+            {
+                title: 'coffee12', 
+                latlng: new kakao.maps.LatLng(35.450936, 126.569477)
+            },
+            {
+                title: 'coffee13', 
+                latlng: new kakao.maps.LatLng(36.450879, 126.569940)
+            },
+            {
+                title: 'coffee14',
+                latlng: new kakao.maps.LatLng(37.451393, 126.570738)
+            }
+        ]
+    },
+    "option2": {
+        'convenience': [
+            {
+                title: '카카오', 
+                latlng: new kakao.maps.LatLng(33.450705, 127.570677)
+            },
+            {
+                title: '생태연못', 
+                latlng: new kakao.maps.LatLng(33.450936, 127.569477)
+            },
+            {
+                title: '텃밭', 
+                latlng: new kakao.maps.LatLng(33.450879, 127.569940)
+            },
+            {
+                title: '근린공원',
+                latlng: new kakao.maps.LatLng(33.451393, 127.570738)
+            }
+        ],
+        'coffee': [
+            {
+                title: 'coffee1', 
+                latlng: new kakao.maps.LatLng(34.450705, 127.570677)
+            },
+            {
+                title: 'coffee12', 
+                latlng: new kakao.maps.LatLng(35.450936, 127.569477)
+            },
+            {
+                title: 'coffee13', 
+                latlng: new kakao.maps.LatLng(36.450879, 127.569940)
+            },
+            {
+                title: 'coffee14',
+                latlng: new kakao.maps.LatLng(37.451393, 127.570738)
+            }
+        ]
+    }
+};
